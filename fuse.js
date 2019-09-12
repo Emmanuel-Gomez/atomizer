c63a45e764353307806ca9cfa7a33a1c6fca141f
Emmanuel Gomez <emmanuel.gomez@lakarleasing.se>
2019-09-12 18:58:17 +0200

    first commit

---
+++ fuse.js
@@ -0,0 +1,63 @@
+const { FuseBox, WebIndexPlugin, SVGPlugin, CSSPlugin, QuantumPlugin } = require("fuse-box");
+const { src, context, task } = require("fuse-box/sparky");
+
+context({
+	isProduction: false,
+	getConfig() {
+		return FuseBox.init({
+			homeDir : "./src",
+			output : "./dist/$name.js",
+			useTypescriptCompiler : true,
+			plugins: [
+				CSSPlugin(),
+				SVGPlugin(),
+				WebIndexPlugin({
+					template : "src/index.html"
+				}),
+				this.isProduction && QuantumPlugin({
+					css : true,
+					treeshake: true,
+					uglify: true
+				})
+			]
+		});
+	},
+	createAppBundle(fuse) {
+		const app = fuse
+			.bundle("app")
+			.instructions(">[index.js]");
+
+			if (!this.isProduction) {
+				app.watch().hmr();
+			}
+
+		return app;
+	},
+	createVendorBundle(fuse) {
+		const app = fuse
+			.bundle("vendor")
+			.instructions("~index.js");
+
+		return app;
+	}
+})
+
+task("clean", () => src("dist").clean("dist").exec() );
+
+task("default", ["clean"], async (context) => {
+	const fuse = context.getConfig();
+
+	fuse.dev({port: 8080});
+	context.createVendorBundle(fuse);
+	context.createAppBundle(fuse);
+	await fuse.run();
+});
+
+task("build", ["clean"], async (context) => {
+	context.isProduction = true;
+	const fuse = context.getConfig();
+
+	context.createVendorBundle(fuse);
+	context.createAppBundle(fuse);
+	await fuse.run();
+});
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+				<h1 className="App-title">Fusebox-React-Boilerplate</h1>
+				</header>
+				<p className="App-intro">
+				</p>
+			</div>
+		);
+	}
+}
+
+export default App;
